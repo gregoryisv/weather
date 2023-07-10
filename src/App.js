1ca42416f6f4d7d744812d49d990a5b32f7a9e68
gregoryisv <novastudio22@gmail.com>
2023-07-11 02:34:46 +0300

    Added animations for background on startup, animations for resizing transitions, added image preloader

--- src/App.js
+++ src/App.js
@@ -11,6 +11,23 @@ function App() {
   const [query, setQuery] = useState({ q:"Санкт-Петербург"});
   const [units, setUnits] = useState("metric");
   const [weather, setWeather] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  
+  const preloadImage = src => 
+  new Promise((resolve, reject) => {
+    const image = new Image()
+    image.onload = resolve
+    image.onerror = reject
+    image.src = src
+  })
+
+  const url = new URL(
+    "https://loremflickr.com/800/1000/forest",
+  );
+
+  url.toString();
+
+  preloadImage("https://loremflickr.com/800/1000/forest").then(()=>setIsLoaded(true))
 
   useEffect(()=>{
     const fetchWeather = () => {
@@ -24,39 +41,40 @@ function App() {
 
   
   const formatBackground = () => {
-    if (!weather){
-      return 'bg-gradient-to-br from-gray-400 to-gray-gray-300'
+    if (!weather || !isLoaded) {
+      return 'bg-gradient-to-br from-gray-400 to-gray-300'
     } else {
-      return 'bg-cover bg-center bg-no-repeat bg-[url("https://loremflickr.com/800/1000/forest")]'
+      if (isLoaded){
+        return 'duration-1000 bg-cover bg-center bg-no-repeat bg-[url("https://loremflickr.com/800/1000/forest")]'
+      }
     }
   }
 
   document.title = "Погода";
-
-  return (
-    <div
-    className={`rounded-xl mx-auto max-w-screen-md my-3 py-5 sm:px-10 lg:px-32 h-fit
-    shadow-xl shadow-gray-400 ${formatBackground()}`}
-    >
+  
+    return (
       <div
-        className='backdrop-blur backdrop-brightness-90
-        p-3 rounded-xl shadow-inner shadow-grey-400'
+      className={`rounded-xl mx-auto max-w-screen-md
+      my-3 py-5 sm:px-10 lg:px-32 h-fit
+      shadow-xl shadow-gray-400 ${formatBackground()}`}
       >
-        
-        <Inputs setQuery={setQuery} units={units} setUnits={setUnits}/>
-
-        {weather && (
-          <div>
-            <TimeAndLoc weather={weather}/>
-            <TempAndDets weather={weather}/>
-            
-            <Forecast title="Почасовой прогноз" items={weather.hourly}/>
-            <Forecast title="Ежедневный прогноз" items={weather.daily}/>
-          </div>
-        )}
+        <div
+          className='backdrop-blur backdrop-brightness-90
+          p-3 rounded-xl'
+        >
+          <Inputs setQuery={setQuery} units={units} setUnits={setUnits}/>
+          {weather && isLoaded && (
+            <div>
+              <TimeAndLoc weather={weather}/>
+              <TempAndDets weather={weather}/>
+              
+              <Forecast title="Почасовой прогноз" items={weather.hourly}/>
+              <Forecast title="Ежедневный прогноз" items={weather.daily}/>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
-  );
+    );
 }
 
 export default App;
